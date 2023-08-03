c9002d0ad247050ce39f0fdd2522faef42bc0ec3
Ellertone <ellertonemain@gmail.com>
2023-08-03 17:25:56 +0300

    Numpy and Opencv

---
+++ OpenCV/faces.py
@@ -0,0 +1,15 @@
+import numpy
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        cv2.imshow("My window", frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord("q"):
+         break
